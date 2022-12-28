1bc64ae1cca95dba7512c9061a11bdf7f271508b
ATamblingPoder <just_a_guy.smfoy@8shield.net>
2022-12-28 16:32:23 +0530

    Added functions: read() print_students() and more ..

--- test-main.c
+++ test-main.c
@@ -1,28 +1,115 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
+#include<string.h>
 
-void teacher(){
-	printf("In teacher!");
-//	char password[] = "librarian123";
-	char password = 'l';
-	printf("  Enter Password: ");
-	printf("\nt> ");
-//	char input[13];
-//	scanf("%s", input);
-//	fgets(input, 13, stdin);
-	char input;
-	scanf("%c", &input);
-	if(input == password){
-		printf("\n  Same Password!");
-	}
+void figletize();
+void librarian();
+void initialize();
+void read();
+void print_students();
+void librarian_options();
+
+int *list_of_ids, number_of_ids = 0;
+struct Student{
+	int self_id, book_id;
+	char name[20], password[20];
+};
+
+void librarian_options(){
+	printf("\n1. Print all students");
+	int choice_librarian_options;
+	printf("\nl> ");
+	scanf("%d", &choice_librarian_options);
+	if(choice_librarian_options)
+		print_students();
 	else
-		printf("Different password!");
+		printf("Invalid Choice!");
 }
+
+void read(){
+	FILE *reader;
+	reader = fopen("students.dat", "rb");
+	struct Student reading_reader;
+	while((fread(&reading_reader, sizeof(reading_reader), 1, reader)) != 0){
+		number_of_ids += 1;
+	}
+	printf("\n%d\n", number_of_ids);
+	fclose(reader);
+}
+
+void print_students(){
+	FILE *printer;
+	printer = fopen("students.dat", "rb");
+	struct Student reading_printer;
+	for(int i = 0; i < number_of_ids; i++){
+		fread(&reading_printer, sizeof(reading_printer), 1, printer);
+		printf("\n%d\t%s\t%d\t%s", reading_printer.self_id, reading_printer.name, reading_printer.book_id, reading_printer.password);
+	}
+	fclose(printer);
+}
+
+void initialize(){
+	struct Student first[3];
+	first[0].self_id = 123456;
+	first[1].self_id = 898595;
+	first[2].self_id = 854808;
+	first[0].book_id = -1;
+	first[1].book_id = -1;
+	first[2].book_id = -1;
+	strcpy(first[0].name, "Ansh Goel");
+	strcpy(first[1].name, "Sparsh Birla");
+	strcpy(first[2].name, "Moulik Srivastava");
+	strcpy(first[0].password, "anshgoel");
+	strcpy(first[1].password, "sparshbirla");
+	strcpy(first[2].password, "mouliksrivastava");
+	FILE *fPtr;
+	fPtr = fopen("students.dat", "rb");
+	if(fPtr == NULL){
+		FILE *fPtr1;
+		fPtr1 = fopen("students.dat", "wb");
+		for(int i = 0; i < 3; i++){
+			fwrite(&(first[i]), sizeof((first[0])), 1, fPtr1);
+		}
+		fclose(fPtr1);
+
+	}
+	else{
+		printf("Exists!");
+		fclose(fPtr);
+	}
+}
+
+void librarian(){
+	figletize();
+	printf("\n------ In librarian() ------\n");
+	char password[] = "librarian123";
+	int correct = 1;
+	while(correct == 1){
+		printf("\nEnter Password: ");
+		printf("\nl> ");
+		char input[13];
+		scanf("%s", input);
+		input[12] = '\0';
+		char dummy = getchar();
+		if(!strcmp(password, input)){
+			figletize();
+			printf("\nl> Login Successfull.\n");
+			librarian_options();
+			correct = 0;
+		}
+		else{
+			printf("\n----|| Invalid password! ||----\n");
+		}
+	}
+}
+
 
 void student(){
-	printf("In Student!");
+	figletize();
+	printf("\n------ In student() ------\n");
 }
+
 
 void animate(){
 	struct timespec ts = {.tv_sec = 0, .tv_nsec = 2.5e7};
@@ -39,21 +126,24 @@ void animate(){
 	}
 }
 
+
 void figletize(){
 	printf("\e[1;1H\e[2J");
 	char figlet[] = "  _     _ _     __  __             ____            \n | |   (_) |__ |  \\/  | __ _ _ __ / ___| _   _ ___ \n | |   | | '_ \\| |\\/| |/ _` | '_ \\\\___ \\| | | / __|\n | |___| | |_) | |  | | (_| | | | |___) | |_| \\__ \\\n |_____|_|_.__/|_|  |_|\\__,_|_| |_|____/ \\__, |___/\n                                         |___/";
 	printf("%s\n", figlet);
+	printf("  Library Management System v 0.0.1\n");
+	printf("---------------------------------------------------------\n");
 }
 
 
 
 int main(){
+	initialize();
+	read();
 	animate();
 	figletize();
-	printf("  Library Management System v 0.0.1\n");
-	printf("---------------------------------------------------------\n");
-	printf("  Teacher or Student?");
-	printf("\n  \"T\" or \"S\" ");
+	printf("  Librarian or Student?");
+	printf("\n  \"L\" or \"S\" ");
 	char choice;
 	int correct = 0;
 	while(correct != 1){
@@ -61,18 +151,16 @@ int main(){
 		scanf("%c", &choice);
 		char dummy;
 		scanf("%c", &dummy);
-		if(choice == 'T' || choice == 't'){
-			teacher();
+		if(choice == 'L' || choice == 'l'){
+			librarian();
 			correct = 1;
 		}
 		else if (choice == 'S' || choice == 's'){
 			student();
 			correct = 1;
 		}
-		else{
+		else
 			printf("\n+++--- Invalid ---+++\n");
-//			correct = 0;
-		}
 	}	
 	return 0;
 }
