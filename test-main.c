c75cb74ca52dae198b8f007b5a8de0d5d1eb22a6
ATamblingPoder <just_a_guy.smfoy@8shield.net>
2022-12-29 18:33:40 +0530

    added clear()

--- test-main.c
+++ test-main.c
@@ -568,7 +568,15 @@ void figletize(){
 	printf("---------------------------------------------------------\n");
 }
 
+void clear(){
+    #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+        system("clear");
+    #endif
 
+    #if defined(_WIN32) || defined(_WIN64)
+        system("cls");
+    #endif
+}
 
 int main(){
 	initialize();
